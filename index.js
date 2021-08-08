f09baee10f0a0c7b05d810b3e206b4c926aaadf9
Jose Julian Salazar Melgar <josejuliansalazarmelgar@gmail.com>
2021-08-07 23:42:10 -0600

    Cambio en el index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb+srv://Admin:123añs@biblioteca.wppj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
 
 { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('Conexión a la base de datos exitosa');
+    console.log('Conexión  correcta a la base de datos exitosa');
 
     app.listen(process.env.PORT || 3000, function() {
         console.log("Servidor utilizando el puerto 3000");
